7de61c44517a57cfe89bd402ab922b1d622d86a7
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-09-19 11:12:25 -0700

    formatting

--- src/ProblemWaterJug.java
+++ src/ProblemWaterJug.java
@@ -6,15 +6,16 @@ import java.util.Set;
  * Created by haodong on 2017-09-17.
  */
 public class ProblemWaterJug extends Problem {
-    boolean goal_test(Object state){
+    boolean goal_test(Object state) {
         //System.out.println(state);
         StateWaterJug jugs_state = (StateWaterJug) state;
         //System.out.println(jugs_state);
-        if(jugs_state.jugs[0] == 1 || jugs_state.jugs[1] == 1 || jugs_state.jugs[2] == 1)
+        if (jugs_state.jugs[0] == 1 || jugs_state.jugs[1] == 1 || jugs_state.jugs[2] == 1)
             return true;
         return false;
     }
-    Set<Object> getSuccessors(Object state){
+
+    Set<Object> getSuccessors(Object state) {
         Set<Object> set = new HashSet<>();
         StateWaterJug jugs_state = (StateWaterJug) state;
 
@@ -26,42 +27,48 @@ public class ProblemWaterJug extends Problem {
         cost = 12 - successor_state.jugs[0];
         successor_state.jugs[0] = 12;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //fill 8 gallon jug
         successor_state = new StateWaterJug(jugs_state);
         cost = 8 - successor_state.jugs[1];
         successor_state.jugs[1] = 8;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //fill 3 gallon jug
         successor_state = new StateWaterJug(jugs_state);
         cost = 3 - successor_state.jugs[2];
         successor_state.jugs[2] = 3;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour out 12 gallon
         successor_state = new StateWaterJug(jugs_state);
         cost = successor_state.jugs[0];
         successor_state.jugs[0] = 0;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour out 8 gallon
         successor_state = new StateWaterJug(jugs_state);
         cost = successor_state.jugs[1];
         successor_state.jugs[1] = 0;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour out 3 gallon
         successor_state = new StateWaterJug(jugs_state);
         cost = successor_state.jugs[2];
         successor_state.jugs[2] = 0;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 12 gallon to 8 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -71,7 +78,8 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[0] = successor_state.jugs[0] - cost;
         successor_state.jugs[1] = successor_state.jugs[1] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 12 gallon to 3 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -81,7 +89,8 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[0] = successor_state.jugs[0] - cost;
         successor_state.jugs[2] = successor_state.jugs[2] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 8 gallon to 3 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -91,7 +100,8 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[1] = successor_state.jugs[1] - cost;
         successor_state.jugs[2] = successor_state.jugs[2] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 8 gallon to 12 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -101,7 +111,8 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[1] = successor_state.jugs[1] - cost;
         successor_state.jugs[0] = successor_state.jugs[0] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 3 gallon to 12 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -111,7 +122,8 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[2] = successor_state.jugs[2] - cost;
         successor_state.jugs[0] = successor_state.jugs[0] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         //pour from 3 gallon to 8 gallon
         successor_state = new StateWaterJug(jugs_state);
@@ -121,32 +133,35 @@ public class ProblemWaterJug extends Problem {
         successor_state.jugs[2] = successor_state.jugs[2] - cost;
         successor_state.jugs[1] = successor_state.jugs[1] + cost;
         successor_state.path_cost += cost;
-        if(isValid(successor_state)) set.add(successor_state);
+        if (isValid(successor_state))
+            set.add(successor_state);
 
         return set;
     }
-    double step_cost(Object fromState, Object toState){
+
+    double step_cost(Object fromState, Object toState) {
         StateWaterJug from = (StateWaterJug) fromState;
         StateWaterJug to = (StateWaterJug) toState;
         return to.path_cost - from.path_cost;
     }
-    public double h(Object state){
+
+    public double h(Object state) {
         return 0;
     }
 
     private boolean isValid(StateWaterJug state) {
-        if(state.jugs[0] <= 12 && state.jugs[1] <= 8 && state.jugs[2] <= 3)
+        if (state.jugs[0] <= 12 && state.jugs[1] <= 8 && state.jugs[2] <= 3)
             return true;
         return false;
     }
 
     public static void main(String[] args) {
         ProblemWaterJug problem = new ProblemWaterJug();
-        int[] jugs = {0, 0, 0};
+        int[] jugs = { 0, 0, 0 };
         problem.initialState = new StateWaterJug(jugs);
         System.out.println(problem.initialState);
 
-        Search search  = new Search(problem);
+        Search search = new Search(problem);
 
         System.out.println("BreadthFirstTreeSearch:\t\t" + search.BreadthFirstTreeSearch());
 
